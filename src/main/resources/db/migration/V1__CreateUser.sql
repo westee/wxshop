65e6328a15020b7f66bbc73753f3fd7964b184a3
westee <904885508@qq.com>
2020-03-11 22:56:49 +0800

    first commit

---
+++ src/main/resources/db/migration/V1__CreateUser.sql
@@ -0,0 +1,8 @@
+create table USER (
+    ID bigint primary key auto_increment,
+    NAME varchar(100),
+    TEL varchar(20) unique,
+    AVATAR_URL varchar(1000),
+    CREATED_AT timestamp,
+    UPDATED_AT timestamp
+)
---
+++ src/test/java/com/westee/wxshop/WxshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westee.wxshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
