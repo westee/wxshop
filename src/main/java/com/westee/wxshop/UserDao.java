991c95bc5ca5d28a34c67e0fd0cc2f6eb797d158
westee <904885508@qq.com>
2020-03-19 22:25:39 +0800

    配置spotbugs

--- src/main/java/com/westee/wxshop/UserDao.java
+++ src/main/java/com/westee/wxshop/UserDao.java
@@ -3,11 +3,13 @@ package com.westee.wxshop;
 import com.westee.wxshop.generate.User;
 import com.westee.wxshop.generate.UserExample;
 import com.westee.wxshop.generate.UserMapper;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@SuppressFBWarnings("RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE")
 @Service
 public class UserDao {
     private final SqlSessionFactory sqlSessionFactory;
