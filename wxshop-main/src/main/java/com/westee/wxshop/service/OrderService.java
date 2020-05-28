1a11c6657fdcb9c8e8ec4a11d6caec70cb704e9d
westee <904885508@qq.com>
2020-05-28 21:39:36 +0800

    下订单模块的RPC实现

--- wxshop-main/src/main/java/com/westee/wxshop/service/OrderService.java
+++ wxshop-main/src/main/java/com/westee/wxshop/service/OrderService.java
@@ -4,16 +4,20 @@ import com.westee.api.data.GoodsInfo;
 import com.westee.api.data.OrderInfo;
 import com.westee.api.generate.Order;
 import com.westee.api.rpc.OrderRpcService;
+import com.westee.wxshop.entity.GoodsWithNumber;
+import com.westee.wxshop.entity.HttpException;
 import com.westee.wxshop.entity.OrderResponse;
 import com.westee.wxshop.generate.Goods;
-import com.westee.wxshop.generate.GoodsExample;
 import com.westee.wxshop.generate.GoodsMapper;
+import com.westee.wxshop.generate.ShopMapper;
 import com.westee.wxshop.generate.UserMapper;
 import org.apache.dubbo.config.annotation.Reference;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -29,11 +33,14 @@ public class OrderService {
 
     private GoodsService goodsService;
 
+    private ShopMapper shopMapper;
+
     @Autowired
-    public OrderService(UserMapper userMapper, GoodsMapper goodsMapper, GoodsService goodsService) {
+    public OrderService(UserMapper userMapper, GoodsMapper goodsMapper, ShopMapper shopMapper, GoodsService goodsService) {
         this.userMapper = userMapper;
         this.goodsMapper = goodsMapper;
         this.goodsService = goodsService;
+        this.shopMapper = shopMapper;
     }
 
     public void deductStock(OrderInfo orderInfo) {
@@ -41,20 +48,53 @@ public class OrderService {
 
     public OrderResponse createOrder(OrderInfo orderInfo, Long userId) {
         Order order = new Order();
-        orderRpcService.createOrder(orderInfo, order);
-        order.setUserId(userId);
-        order.setAddress(userMapper.selectByPrimaryKey(userId).getAddress());
-        order.setTotalPrice(calculateTotalPrice(orderInfo));
-        return null;
-    }
-
-    private BigDecimal calculateTotalPrice(OrderInfo orderInfo) {
         List<Long> goodsId = orderInfo.getGoods()
                 .stream()
                 .map(GoodsInfo::getId)
                 .collect(Collectors.toList());
         Map<Long, Goods> idToGoodsMap = goodsService.getIdToGoodsMap(goodsId);
 
-        return idToGoodsMap;
+        orderRpcService.createOrder(orderInfo, order);
+        order.setUserId(userId);
+        order.setAddress(userMapper.selectByPrimaryKey(userId).getAddress());
+        order.setTotalPrice(calculateTotalPrice(orderInfo, idToGoodsMap));
+
+        Order createOrder = orderRpcService.createOrder(orderInfo, order);
+
+        OrderResponse response = new OrderResponse(createOrder);
+        Long shopId = new ArrayList<>(idToGoodsMap.values()).get(0).getShopId();
+        response.setShop(shopMapper.selectByPrimaryKey(shopId));
+
+        response.setGoods(
+                orderInfo.getGoods()
+                        .stream()
+                        .map(goods -> toGoodsWithNumber(goods, idToGoodsMap))
+                        .collect(Collectors.toList()));
+        return response;
+    }
+
+
+    private GoodsWithNumber toGoodsWithNumber(GoodsInfo goodsInfo, Map<Long, Goods> idToGoodsMap) {
+        GoodsWithNumber result = new GoodsWithNumber(idToGoodsMap.get(goodsInfo.getId()));
+        result.setNumber(goodsInfo.getNumber());
+        return result;
+    }
+
+
+    private BigDecimal calculateTotalPrice(OrderInfo orderInfo, Map<Long, Goods> idToGoodsMap) {
+
+        BigDecimal result = BigDecimal.ZERO;
+        for (GoodsInfo goodsInfo : orderInfo.getGoods()) {
+            Goods goods = idToGoodsMap.get(goodsInfo.getId());
+            if (goods == null) {
+                throw HttpException.badRequest("id非法" + goodsInfo.getId());
+            }
+            if (goodsInfo.getNumber() <= 0) {
+                throw HttpException.badRequest("number非法" + goodsInfo.getNumber());
+            }
+
+            result = result.add(goods.getPrice().multiply(new BigDecimal(goodsInfo.getNumber())));
+        }
+        return result;
     }
 }
